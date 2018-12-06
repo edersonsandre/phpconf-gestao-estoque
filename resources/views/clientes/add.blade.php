8757f9c7a922011e33c9f902971bd952998647fd
Ederson Sandre <ederson.sandre@gmail.com>
2018-12-06 15:42:52 -0200

    Aula

--- resources/views/clientes/add.blade.php
+++ resources/views/clientes/add.blade.php
@@ -4,11 +4,17 @@
 
     <form>
 
+        <div class="row">
+            <div class="col-md-6">{!! Form::cText('nome', 'Nome') !!}</div>
+            <div class="col-md-6">{!! Form::cText('email', 'Email') !!}</div>
+            <div class="col-md-6">{!! Form::cText('telefone', 'Telefone') !!}</div>
+            <div class="col-md-6">{!! Form::cText('cpf', 'CPF') !!}</div>
+        </div>
 
-        {!! Form::cText('nome', 'Nome') !!}
+        <div >
+            <button type="submit" class="btn btn-success">Gravar</button>
+        </div>
 
-        Nome
-        <input type="nome"  />
 
     </form>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,16 @@
+<html >
+
+<head>
+    <link href="{!! asset('src/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+</head>
+
+
 
 <a href="{!! route('clientes') !!}" >Clientes</a>
 
 <hr />
 
 @yield('content')
+
+
+</html>
