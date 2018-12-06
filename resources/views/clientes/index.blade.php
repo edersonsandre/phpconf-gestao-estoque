90153db759e0e0f3cb69155549e019e70b7a2a0b
Ederson Sandre <ederson.sandre@gmail.com>
2018-12-06 16:16:25 -0200

    Aula

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -2,28 +2,38 @@
 
 @section('content')
 
-    <div style="text-align: right">
-        <a href="{!! route('clientes-add') !!}">Novo Registro</a>
+    <div style="margin: 20px 50px">
+
+        <div style="text-align: right">
+            <a href="{!! route('clientes-add') !!}">Novo Registro</a>
+        </div>
+
+
+        <table width="100%" border="1">
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>CPF</th>
+                <th>Ações</th>
+            </tr>
+
+            @foreach($clientes AS $cliente)
+                <tr>
+                    <td>{!! $cliente->id !!}</td>
+                    <td>{!! $cliente->nome !!}</td>
+                    <td>{!! $cliente->telefone !!}</td>
+                    <td>{!! $cliente->cpf !!}</td>
+                    <td>
+                        <a href="{!! route('clientes-edit', ['id', $cliente->id]) !!}"
+                           class="btn btn-info">
+                            E
+                        </a>
+                    </td>
+                </tr>
+
+            @endforeach
+        </table>
+
     </div>
-
-
-    <table width="100%" border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Telefone</th>
-            <th>CPF</th>
-        </tr>
-
-    @foreach($clientes AS $cliente)
-        <tr>
-            <td>{!! $cliente->id !!}</td>
-            <td>{!! $cliente->nome !!}</td>
-            <td>{!! $cliente->telefone !!}</td>
-            <td>{!! $cliente->cpf !!}</td>
-        </tr>
-
-    @endforeach
-    </table>
-
 @stop
